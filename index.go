b7e6b9956f593814202d22df27c24adaab0f2577
TeamChii <worapong4712@gmail.com>
2019-01-16 10:46:03 +0700

    go api test

---
+++ index.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+
+	router := mux.NewRouter()
+	userDB := map[string]int{
+		"team":  23,
+		"park":  21,
+		"poom":  30,
+		"first": 44,
+	}
+
+	log.Print("Starting the service")
+
+	////// ส่งค่าเป็น json
+	router.HandleFunc("/api", func(w http.ResponseWriter, r *http.Request) {
+
+		w.Header().Set("Content-type", "application/json")
+		fmt.Fprintf(w, `{"id":"aa"}`)
+	})
+
+	/// router  ธรรมดา
+	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "home.html")
+	})
+	router.HandleFunc("/signup", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "signup.html")
+	})
+	router.HandleFunc("/login", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "Login.html")
+	})
+	router.HandleFunc("/file", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "name.txt")
+	})
+
+	// ส่งพารามิเตอร์
+	router.HandleFunc("/user/{name}", func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		name := vars["name"]
+		age := userDB[name]
+		fmt.Fprintf(w, "My name is %s %d", name, age)
+	}).Methods("GET")
+
+	log.Print("The service is ready to listen and serve.")
+	log.Fatal(http.ListenAndServe(":8000", router))
+}
